151c43cf71df60cfdd88cb89861c218e6f43e5b7
Tayyab Cheema <116276512+tayyabcheema@users.noreply.github.com>
2023-11-05 18:36:58 +0500

    final commit

---
+++ pf.py
@@ -0,0 +1,113 @@
+# Print line command
+# print("Hello World")
+
+# Declaring variables 
+# name = "Tayyab Cheema"
+# age = 22
+# isEmployee = False
+# print(name,age, isEmployee)
+
+# Taking input from user
+# username = input("Enter username:")
+# print("Username is: " + username)
+
+# Type conversions in Python
+# age = input("Enter your age ? ")
+# new_age = int(age) + 2
+# print(new_age)
+
+# first = input("Enter 1st number: ")
+# second = input("Enter 2nd number: ")
+
+# sum = int(first) + int(second)
+
+# print("The sum of 2 numbers is: " + str(sum))
+
+# IF-ELSE Statement 
+
+# age = 16  
+# if age>18:
+#     print("You are an adult") 
+# elif age>18 and age <30:
+#     print("You are in University")
+
+# MINI Project Calculator using If-else
+
+# first = input("Enter 1st number : ")
+# operator = input("Select an operator +,-,*,/,% : ")
+# second = input("Enter 2nd number : ")
+
+# # COnverting the type of input numbers from string to integer
+# first = int(first)
+# second = int(second)
+
+# if operator == "+":
+#     print(first + second)
+# elif operator == "-":
+#     print(first - second)
+# elif operator == "*":
+#     print(first * second)
+# elif operator == "/":
+#     print(first / second)s
+# elif operator == "%":
+#     print(first % second)
+# else:
+#     print("Invalid operator")
+
+# LOOPS
+# While Loop
+
+# i = 1
+# while i < 6:
+#   print(i * "*")
+#   i = i + 1
+
+# Reverse the pattern using while loop
+# i = 6
+# while i >= 1:
+#   print(i * "*")
+#   i = i - 1
+
+# For loop
+
+# for i in range(5):
+#     print(i)
+
+# List Data Type in Python
+
+# marks = [99,93,88,80,82]
+
+# print(marks)
+
+# Break and Continue
+# students = ["Tayyab", "Ahmed", "Hamid", "Ali", "Hassan", "Zeeshan"]
+# for student in students:
+#     if student == "Ali":
+#         break
+#     print(student)
+
+# for student in students:
+#     if student == "Hassan":
+#         continue
+#     print(student)
+
+# Tuple
+# same as list written in ()
+
+# # Set 
+# same as list and tuple but in {}
+
+# Dictionary
+
+# marks = {"English" : 90, "Physics" : 70}
+# print(marks["English"])
+
+# marks["TOA"] = 84
+
+# print(marks)
+
+# Functions
+
+# def sum(first, second):
+#     print(first + second)
+# sum(1,7)
